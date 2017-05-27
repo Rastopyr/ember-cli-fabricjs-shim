fafafcf619210cc4875bc10a5090d8c41239fbc1
Сенин Роман <rastopyr@gmail.com>
2017-05-28 02:50:06 +0300

    [Finish] Shim to Fabric.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-fabricjs-shim'
+  name: 'ember-cli-fabricjs-shim',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    const vendor = this.treePaths.vendor;
+
+    app.import(vendor + '/fabric.require.js', { prepend: true });
+    app.import(vendor + '/shims/fabric.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var fabricTree = new Funnel(path.join(this.project.root, 'node_modules', 'fabric', 'dist'), {
+      files: ['fabric.require.js']
+    });
+
+    return new MergeTrees([vendorTree, fabricTree]);
+  },
 };
---
+++ tests/unit/utils/fabricjs-import-test.js
@@ -0,0 +1,10 @@
+
+import Fabric from 'fabric';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fabricjs import');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  assert.ok(Fabric);
+});
